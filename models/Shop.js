d23cebd44b5641b535af8606ecb03bf6df93deee
nbkjohu <navdeepjangra1988@gmail.com>
2025-01-23 11:10:07 +0530

    Recreate models folder and Shop.js file

---
+++ models/Shop.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const shopSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    offer: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Shop", shopSchema);
